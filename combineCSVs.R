bb13e1000ce4e66b0c01fd06f2b844c11a5ac3b8
hlanier14 <60249654+hlanier14@users.noreply.github.com>
2021-09-10 20:57:13 -0500

    Combined city data csvs into "cities.csv"

---
+++ combineCSVs.R
@@ -0,0 +1,16 @@
+
+koppen <- read.csv("koppen.csv", header = TRUE)
+latlong <- read.csv("latlong.csv")
+
+test <- merge(latlong, koppen, by=c("CITY"))
+test[which(test["STATE.x"] != test["STATE.y"]),]
+
+cities <- merge(latlong, koppen, by=c("CITY","STATE"))
+cities
+
+library(stringr)
+
+cities$LAT <- paste(str_sub(cities$LAT, 1, -3), str_sub(cities$LAT, -1, -1), sep="")
+cities$LON <- paste(str_sub(cities$LON, 1, -3), str_sub(cities$LON, -1, -1), sep="")
+
+write.csv(cities, file="cities.csv", row.names=F)
