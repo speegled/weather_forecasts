9825324b1962e2e8db0f28cbf978fbdcfcf7c043
Sai-56 <bsaishreyas@gmail.com>
2021-10-23 16:34:23 -0500

    updating weather scraper with new cols

--- Scraper/Code/weatherScraper.R
+++ Scraper/Code/weatherScraper.R
@@ -1,17 +1,186 @@
 library(rvest)
 library(dplyr)
 library(stringr)
-a = read.csv("koppen.csv")
-df = data.frame(matrix(nrow = 0, ncol = 9))
-names(df) = c("City",
-              "State",
-              "Date",
-              "D0_Hi",
-              "D0_Lo",
-              "D1_Hi",
-              "D1_Lo",
-              "D2_Hi",
-              "D2_Lo")
+library(lubridate)
+a = read.csv("Scraper/Data/koppen.csv")
+df = data.frame(matrix(nrow = 0, ncol = 10))
+colNames = c(
+  "date",
+  "city",
+  "state",
+  "forecast_lo_2_prev_PM",
+  "forecast_hi_2_prev_PM",
+  "forecast_lo_prev_AM",
+  "forecast_hi_prev_AM",
+  "forecast_lo_prev_PM",
+  "forecast_hi_prev_PM",
+  "forecast_lo_current_AM",
+  "forecast_hi_current_AM",
+  "actual_lo_current_PM",
+  "actual_hi_current_PM",
+  "actual_lo_next_AM",
+  "actual_hi_next_AM",
+  "forecast_lo_next_AM",
+  "forecast_hi_next_AM",
+  "forecast_lo_next_PM",
+  "forecast_hi_next_",
+  "forecast_lo_2_next_AM",
+  "forecast_hi_2_next_AM"
+  
+)
+names(df) =  c(
+  "date",
+  "time",
+  "city",
+  "state",
+  "forecast_lo_2_prev_PM",
+  "forecast_hi_2_prev_PM",
+  "forecast_lo_prev_AM",
+  "forecast_hi_prev_AM" ,
+  "forecast_lo_prev_PM" ,
+  "forecast_hi_prev_PM" ,
+  "forecast_lo_current_AM",
+  "forecast_hi_current_AM",
+  "forecast_lo_current_PM",
+  "forecast_hi_curent_PM",
+  "actual_lo_current_PM",
+  "actual_hi_current_PM",
+  "actual_lo_next_AM",
+  "actual_hi_next_AM",
+  "forecast_lo_next_AM",
+  "forecast_hi_next_AM",
+  "forecast_lo_next_PM",
+  "forecast_hi_next_PM",
+  "forecast_lo_2_next_AM",
+  "forecast_hi_2_next_AM",
+  "forecast_lo_2_next_PM",
+  "forecast_hi_2_next_PM",
+)
+func = function(fcast) {
+  dttm = parse_date_time(as.character(date()),
+                         "%a %b %d %H:%M:%S %Y")
+  dt = date(dttm)
+  tm = paste(hour(dttm), minute(dttm), second(dttm), sep = ":")
+  df_temp = tryCatch({
+    fcast = strsplit(fcast, "\n")[[1]]
+    fcast = str_replace_all(fcast, pattern = "[\r\n]", replacement = " ")
+    fcast = fcast[fcast != ""]
+    fcast = fcast[fcast != " "]
+    
+    
+    while (fcast[1] != "Today" & fcast[1] !=  "Tonight") {
+      fcast = fcast[-1]
+    }
+    if (fcast[1] == "Today")
+    {
+      df_temp = data.frame(
+        "date" = dt,
+        "time" = tm,
+        "city" = a[i, 1],
+        "state" = a[i, 2],
+        "forecast_lo_2_prev_PM" = NA,
+        "forecast_hi_2_prev_PM" = NA,
+        "forecast_lo_prev_AM" = NA,
+        "forecast_hi_prev_AM" = NA,
+        "forecast_lo_prev_PM" = NA,
+        "forecast_hi_prev_PM" = NA,
+        "forecast_lo_current_AM" = NA,
+        "forecast_hi_current_AM" = str_extract(fcast[2], "[[:digit:]]+"),
+        #today high ^
+        "forecast_lo_current_PM" = str_extract(fcast[4], "[[:digit:]]+"),
+        #today lo
+        "forecast_hi_curent_PM" = NA,
+        "actual_lo_current_PM" = NA,
+        "actual_hi_current_PM" = NA,
+        "actual_lo_next_AM" = NA,
+        "actual_hi_next_AM" = NA,
+        "forecast_lo_next_AM" = NA,
+        "forecast_hi_next_AM" = str_extract(fcast[6], "[[:digit:]]+"),
+        #tom hi,
+        "forecast_lo_next_PM" = str_extract(fcast[8], "[[:digit:]]+"),
+        # tom lo
+        "forecast_hi_next_PM" = NA,
+        "forecast_lo_2_next_AM" = NA,
+        "forecast_hi_2_next_AM" = str_extract(fcast[10], "[[:digit:]]+"),
+        # day aft hi
+        "forecast_lo_2_next_PM" = str_extract(fcast[12], "[[:digit:]]+"),
+        # day aft lo
+        "forecast_hi_2_next_PM" = NA
+      )
+    } else{
+      df_temp = data.frame(
+        "date" = dt,
+        "time" = tm,
+        "city" = a[i, 1],
+        "state" = a[i, 2],
+        "forecast_lo_2_prev_PM" = NA,
+        "forecast_hi_2_prev_PM" = NA,
+        "forecast_lo_prev_AM" = NA,
+        "forecast_hi_prev_AM" = NA,
+        "forecast_lo_prev_PM" = NA,
+        "forecast_hi_prev_PM" = NA,
+        "forecast_lo_current_AM" = NA,
+        "forecast_hi_current_AM" = NA,
+        #today high
+        "forecast_lo_current_PM" = str_extract(fcast[2], "[[:digit:]]+"),
+        #today lo
+        "forecast_hi_curent_PM" = NA,
+        "actual_lo_current_PM" = NA,
+        "actual_hi_current_PM" = NA,
+        "actual_lo_next_AM" = NA,
+        "actual_hi_next_AM" = NA,
+        "forecast_lo_next_AM" = NA,
+        "forecast_hi_next_AM" = str_extract(fcast[4], "[[:digit:]]+"),
+        #tom hi,
+        "forecast_lo_next_PM" = str_extract(fcast[6], "[[:digit:]]+"),
+        # tom lo
+        "forecast_hi_next_PM" = NA,
+        "forecast_lo_2_next_AM" = NA,
+        "forecast_hi_2_next_AM" = str_extract(fcast[8], "[[:digit:]]+"),
+        # day aft hi
+        "forecast_lo_2_next_PM" = str_extract(fcast[10], "[[:digit:]]+"),
+        # day aft lo
+        "forecast_hi_2_next_PM" = NA
+      )
+    
+    }
+    
+  },
+  error = function(cond) {
+    message(paste("Could not fetch", a[i, 1], a[i, 2], sep = " "))
+    message(paste(cond), "\n")
+    message(paste("forecast:: ", fcast, sep = "---"))
+    df_temp = data.frame(
+      "date" = dt,
+      "time" = tm,
+      "city" = a[i, 1],
+      "state" = a[i, 2],
+      "forecast_lo_2_prev_PM" = NA,
+      "forecast_hi_2_prev_PM" = NA,
+      "forecast_lo_prev_AM" = NA,
+      "forecast_hi_prev_AM" = NA,
+      "forecast_lo_prev_PM" = NA,
+      "forecast_hi_prev_PM" = NA,
+      "forecast_lo_current_AM" = NA,
+      "forecast_hi_current_AM" = NA,  #today high
+      "forecast_lo_current_PM" = NA,      #today lo
+      "forecast_hi_curent_PM" = NA,
+      "actual_lo_current_PM" = NA,
+      "actual_hi_current_PM" = NA,
+      "actual_lo_next_AM" = NA,
+      "actual_hi_next_AM" = NA,
+      "forecast_lo_next_AM" = NA,
+      "forecast_hi_next_AM" = NA,      #tom hi,
+      "forecast_lo_next_PM" = NA,      # tom lo
+      "forecast_hi_next_PM" = NA,
+      "forecast_lo_2_next_AM" = NA,
+      "forecast_hi_2_next_AM" = NA,      # day aft hi
+      "forecast_lo_2_next_PM" = NA,      # day aft lo
+      "forecast_hi_2_next_PM" = NA
+    )
+  })
+df_temp
+}
 
 for (i in 1:128) {
   url <- 'https://www.weather.gov/'
@@ -35,76 +204,23 @@ for (i in 1:128) {
     session = follow_link(session,
                           s3 %>% html_element("table") %>% html_element("a") %>% html_text())
   }
-  # s4 = session2 %>% html_nodes(".panel-title") %>% html_text2()
-  # gsub("[\\(\\)]", "", regmatches(s3, gregexpr("\\(.*?\\)", s3))[[1]])
-  # Sys.sleep(10)
   forecast <-
     html_node(session, css = "#seven-day-forecast-list") %>%
     html_text()
   
   df_temp = func(forecast)
-
+  
   df = rbind(df, df_temp)
-
+  
 }
 
-func = function(fcast) {
-  df_temp = tryCatch({
-    fcast = strsplit(fcast, "\n")[[1]]
-    fcast = str_replace_all(fcast, pattern = "[\r\n]", replacement = " ")
-    fcast = fcast[fcast != ""]
-    fcast = fcast[fcast != " "]
-    
-    while (fcast[1] != "Today" & fcast[1] !=  "Tonight") {
-      fcast = fcast[-1]
-    }
-    if (fcast[1] == "Today")
-    {
-      df_temp = data.frame(
-        "City" = a[i, 1],
-        "State" = a[i, 2],
-        "date" = date(),
-        "D0_Hi" = str_extract(fcast[2], "[[:digit:]]+"),
-        "D0_Lo" = str_extract(fcast[4], "[[:digit:]]+"),
-        "D1_Hi" = str_extract(fcast[6], "[[:digit:]]+"),
-        "D1_Lo" = str_extract(fcast[8], "[[:digit:]]+"),
-        "D2_Hi" = str_extract(fcast[10], "[[:digit:]]+"),
-        "D2_Lo" = str_extract(fcast[12], "[[:digit:]]+")
-      )
-    } else{
-      df_temp = data.frame(
-        "City" = a[i, 1],
-        "State" = a[i, 2],
-        "date" = date(),
-        "D0_Hi" = NA,
-        "D0_Lo" = str_extract(fcast[2], "[[:digit:]]+"),
-        "D1_Hi" = str_extract(fcast[4], "[[:digit:]]+"),
-        "D1_Lo" = str_extract(fcast[6], "[[:digit:]]+"),
-        "D2_Hi" = str_extract(fcast[8], "[[:digit:]]+"),
-        "D2_Lo" = str_extract(fcast[10], "[[:digit:]]+")
-      )
-    }
-    
-  },
-  error = function(cond){
-    message(paste("Could not fetch", a[i, 1], a[i, 2], sep = " "))
-    message(paste(cond),"\n")
-    message(paste("forecast:: ", fcast, sep = "---"))
-    df_temp = data.frame(
-      "City" = a[i, 1],
-      "State" = a[i, 2],
-      "date" = date(),
-      "D0_Hi" = NA,
-      "D0_Lo" = NA,
-      "D1_Hi" = NA,
-      "D1_Lo" = NA,
-      "D2_Hi" = NA,
-      "D2_Lo" = NA
-    )
-  })
-  df_temp
-}
-
-write.table(df, "weather_data.csv" , append = TRUE, sep = "," , col.names = !file.exists("weather_data.csv"), row.names = FALSE)
 
 
+write.table(
+  df,
+  "Scraper/data/weather_data.csv" ,
+  append = TRUE,
+  sep = "," ,
+  col.names = !file.exists("Scraper/data/weather_data.csv"),
+  row.names = FALSE
+)
